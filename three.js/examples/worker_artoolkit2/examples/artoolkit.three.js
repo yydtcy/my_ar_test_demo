386b79a992fcb2bed01dad0a1c01eb9104e79cbb
Ren Zijian <you@example.com>
2019-03-11 14:02:01 +0800

    提交信息

--- three.js/examples/worker_artoolkit2/examples/artoolkit.three.js
+++ three.js/examples/worker_artoolkit2/examples/artoolkit.three.js
@@ -5,22 +5,31 @@
 		/**
 			Helper for setting up a Three.js AR scene using the device camera as input.
 			Pass in the maximum dimensions of the video you want to process and onSuccess and onError callbacks.
+
 			On a successful initialization, the onSuccess callback is called with an ThreeARScene object.
 			The ThreeARScene object contains two THREE.js scenes (one for the video image and other for the 3D scene)
 			and a couple of helper functions for doing video frame processing and AR rendering.
+
 			Here's the structure of the ThreeARScene object:
 			{
 				scene: THREE.Scene, // The 3D scene. Put your AR objects here.
 				camera: THREE.Camera, // The 3D scene camera.
+
 				arController: ARController,
+
 				video: HTMLVideoElement, // The userMedia video element.
+
 				videoScene: THREE.Scene, // The userMedia video image scene. Shows the video feed.
 				videoCamera: THREE.Camera, // Camera for the userMedia video scene.
+
 				process: function(), // Process the current video frame and update the markers in the scene.
 				renderOn: function( THREE.WebGLRenderer ) // Render the AR scene and video background on the given Three.js renderer.
 			}
+
 			You should use the arScene.video.videoWidth and arScene.video.videoHeight to set the width and height of your renderer.
+
 			In your frame loop, use arScene.process() and arScene.renderOn(renderer) to do frame processing and 3D rendering, respectively.
+
 			@param {number} width - The maximum width of the userMedia video to request.
 			@param {number} height - The maximum height of the userMedia video to request.
 			@param {function} onSuccess - Called on successful initialization with an ThreeARScene object.
@@ -44,21 +53,30 @@
 
 		/**
 			Creates a Three.js scene for use with this ARController.
+
 			Returns a ThreeARScene object that contains two THREE.js scenes (one for the video image and other for the 3D scene)
 			and a couple of helper functions for doing video frame processing and AR rendering.
+
 			Here's the structure of the ThreeARScene object:
 			{
 				scene: THREE.Scene, // The 3D scene. Put your AR objects here.
 				camera: THREE.Camera, // The 3D scene camera.
+
 				arController: ARController,
+
 				video: HTMLVideoElement, // The userMedia video element.
+
 				videoScene: THREE.Scene, // The userMedia video image scene. Shows the video feed.
 				videoCamera: THREE.Camera, // Camera for the userMedia video scene.
+
 				process: function(), // Process the current video frame and update the markers in the scene.
 				renderOn: function( THREE.WebGLRenderer ) // Render the AR scene and video background on the given Three.js renderer.
 			}
+
 			You should use the arScene.video.videoWidth and arScene.video.videoHeight to set the width and height of your renderer.
+
 			In your frame loop, use arScene.process() and arScene.renderOn(renderer) to do frame processing and 3D rendering, respectively.
+
 			@param video Video image to use as scene background. Defaults to this.image
 		*/
 		ARController.prototype.createThreeScene = function(video) {
@@ -155,12 +173,15 @@
 		/**
 			Creates a Three.js marker Object3D for the given marker UID.
 			The marker Object3D tracks the marker pattern when it's detected in the video.
+
 			Use this after a successful artoolkit.loadMarker call:
+
 			arController.loadMarker('/bin/Data/patt.hiro', function(markerUID) {
 				var markerRoot = arController.createThreeMarker(markerUID);
 				markerRoot.add(myFancyHiroModel);
 				arScene.scene.add(markerRoot);
 			});
+
 			@param {number} markerUID The UID of the marker to track.
 			@param {number} markerWidth The width of the marker, defaults to 1.
 			@return {THREE.Object3D} Three.Object3D that tracks the given marker.
@@ -177,12 +198,15 @@
 		/**
 			Creates a Three.js marker Object3D for the given NFT marker UID.
 			The marker Object3D tracks the NFT marker when it's detected in the video.
+
 			Use this after a successful artoolkit.loadNFTMarker call:
+
 			arController.loadNFTMarker('DataNFT/pinball', function(markerUID) {
 				var markerRoot = arController.createThreeNFTMarker(markerUID);
 				markerRoot.add(myFancyModel);
 				arScene.scene.add(markerRoot);
 			});
+
 			@param {number} markerUID The UID of the marker to track.
 			@param {number} markerWidth The width of the marker, defaults to 1.
 			@return {THREE.Object3D} Three.Object3D that tracks the given marker.
@@ -199,12 +223,15 @@
 		/**
 			Creates a Three.js marker Object3D for the given multimarker UID.
 			The marker Object3D tracks the multimarker when it's detected in the video.
+
 			Use this after a successful arController.loadMarker call:
+
 			arController.loadMultiMarker('/bin/Data/multi-barcode-4x3.dat', function(markerUID) {
 				var markerRoot = arController.createThreeMultiMarker(markerUID);
 				markerRoot.add(myFancyMultiMarkerModel);
 				arScene.scene.add(markerRoot);
 			});
+
 			@param {number} markerUID The UID of the marker to track.
 			@return {THREE.Object3D} Three.Object3D that tracks the given marker.
 		*/
@@ -220,12 +247,15 @@
 		/**
 			Creates a Three.js marker Object3D for the given barcode marker UID.
 			The marker Object3D tracks the marker pattern when it's detected in the video.
+
 			var markerRoot20 = arController.createThreeBarcodeMarker(20);
 			markerRoot20.add(myFancyNumber20Model);
 			arScene.scene.add(markerRoot20);
+
 			var markerRoot5 = arController.createThreeBarcodeMarker(5);
 			markerRoot5.add(myFancyNumber5Model);
 			arScene.scene.add(markerRoot5);
+
 			@param {number} markerUID The UID of the barcode marker to track.
 			@param {number} markerWidth The width of the marker, defaults to 1.
 			@return {THREE.Object3D} Three.Object3D that tracks the given marker.
